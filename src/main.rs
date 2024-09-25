db3534d664bc50344dfdb8894cf25887b4c02f21
ekuinox <depkey@me.com>
2024-09-26 00:56:32 +0900

    WIP: とりあえずログインできる

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,34 @@ mod routes;
 mod templates;
 
 use axum::{routing::get, Router};
+use rspotify::Credentials;
+use shuttle_runtime::SecretStore;
 
 use crate::routes::AppState;
 
 #[shuttle_runtime::main]
-async fn main() -> shuttle_axum::ShuttleAxum {
+async fn main(#[shuttle_runtime::Secrets] secrets: SecretStore) -> shuttle_axum::ShuttleAxum {
+    let spotify_client_id = secrets
+        .get("SPOTIFY_CLIENT_ID")
+        .expect("`SPOTIFY_CLIENT_ID` is empty.");
+    let spotify_client_secret = secrets
+        .get("SPOTIFY_CLIENT_SECRET")
+        .expect("`SPOTIFY_CLIENT_SECRET is empty.`");
+    let spotify_redirect_uri = secrets
+        .get("SPOTIFY_REDIRECT_URI")
+        .expect("`SPOTIFY_REDIRECT_URI is empty.`");
+
+    let credentials = Credentials::new(&spotify_client_id, &spotify_client_secret);
+
     let router = Router::new()
         .route("/", get(routes::get_index))
-        .with_state(AppState {});
+        .route("/spotify/auth", get(routes::get_spotify_auth))
+        .route("/spotify/callback", get(routes::get_spotify_callback))
+        .with_state(AppState {
+            credentials,
+            callback_uri: spotify_redirect_uri,
+            pkces: Default::default(),
+        });
 
     Ok(router.into())
 }
--- src/routes.rs
+++ src/routes.rs
@@ -1,10 +1,97 @@
-use axum::response::IntoResponse;
+use std::{collections::HashMap, sync::Arc};
+
+use anyhow::{Context as _, Result};
+use axum::{
+    extract::{Query, State},
+    http::StatusCode,
+    response::{IntoResponse, Redirect},
+};
+use rspotify::{prelude::OAuthClient as _, scopes, AuthCodePkceSpotify, Credentials, OAuth};
+use serde::Deserialize;
+use shuttle_runtime::tokio::sync::RwLock;
 
 use crate::templates::IndexTemplate;
 
-#[derive(Default, PartialEq, Clone)]
-pub struct AppState {}
+#[derive(Clone)]
+pub struct AppState {
+    pub callback_uri: String,
+    pub credentials: Credentials,
+    pub pkces: Arc<RwLock<HashMap<String, AuthCodePkceSpotify>>>,
+}
+
+impl AppState {
+    pub async fn authorize_url(&self) -> Result<String> {
+        let oauth = OAuth {
+            redirect_uri: self.callback_uri.clone(),
+            scopes: scopes!(
+                "user-read-currently-playing",
+                "playlist-modify-private",
+                "user-top-read"
+            ),
+            ..Default::default()
+        };
+
+        let state = oauth.state.clone();
+
+        let mut pkce = AuthCodePkceSpotify::new(self.credentials.clone(), oauth);
+        let url = pkce.get_authorize_url(Default::default())?;
+
+        let mut pkces = self.pkces.write().await;
+        pkces.insert(state, pkce);
+
+        Ok(url)
+    }
+
+    pub async fn pkce(&self, state: &str) -> Result<AuthCodePkceSpotify> {
+        let pkce = self
+            .pkces
+            .write()
+            .await
+            .remove(state)
+            .context("Not found")?;
+        Ok(pkce)
+    }
+}
 
 pub async fn get_index() -> impl IntoResponse {
     IndexTemplate
+}
+
+pub async fn get_spotify_auth(
+    State(state): State<AppState>,
+) -> Result<impl IntoResponse, (StatusCode, String)> {
+    let url = state
+        .authorize_url()
+        .await
+        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+
+    Ok(Redirect::temporary(&url))
+}
+
+#[derive(Deserialize)]
+pub struct GetSpotifyCallbackQuery {
+    pub code: String,
+    pub state: String,
+}
+
+pub async fn get_spotify_callback(
+    State(state): State<AppState>,
+    Query(query): Query<GetSpotifyCallbackQuery>,
+) -> Result<impl IntoResponse, (StatusCode, String)> {
+    let pkce = state
+        .pkce(&query.state)
+        .await
+        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+    pkce.request_token(&query.code)
+        .await
+        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+
+    let me = pkce
+        .me()
+        .await
+        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+
+    dbg!(&me);
+
+    Ok("")
 }
